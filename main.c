9cb8f15d16ea1956e17d6541c1778ed811c32703
Ramirouf <ramirouf@gmail.com>
2022-06-28 18:33:15 -0300

    Fixed small conflict in comments

--- main.c
+++ main.c
@@ -13,7 +13,8 @@
 #include <conio.h>
 // Testing
 
-struct ConfigTicket *GetConfigTicket(){
+struct ConfigTicket *GetConfigTicket()
+{
 	struct ConfigTicket *config = malloc(sizeof(struct ConfigTicket));
 	FILE *fptr;
 	char *filename = "./public/ticket_config.txt";
@@ -51,13 +52,15 @@ struct ConfigTicket *GetConfigTicket(){
 	return config;
 }
 
-void PreviewTicket(){
+void PreviewTicket()
+{
 	// char *mode = "r";
 	// // Ticket data
 	// struct ConfigTicket *configT = GetConfigTicket(mode);
 }
 
-void PrintConfigTicket(){
+void PrintConfigTicket()
+{
 	// struct ConfigTicket *configT = GetConfigTicket();
 
 	// Print the text from the file.
@@ -72,7 +75,8 @@ void PrintConfigTicket(){
 	printf("\n");
 }
 
-void SetConfigTicket(){
+void SetConfigTicket()
+{
 	char *filename = "./public/ticket_config.txt";
 	char *mode = "w";
 
@@ -122,10 +126,12 @@ void SetConfigTicket(){
 	fclose(fptr);
 }
 
-void TicketSettings(){
+void TicketSettings()
+{
 	int option;
 
-	do{
+	do
+	{
 		MenuTitle("CONFIGURACION DE TICKET");
 		printf("1) Ver informacion\n");
 		printf("2) Establecer informacion\n");
@@ -137,60 +143,74 @@ void TicketSettings(){
 		printf("Ingrese una opcion: ");
 		scanf("%d", &option);
 
-		switch (option){
-			case 1:
-				if (setTicket){
-					printf("\n");
-					PrintConfigTicket();
-					printf("\n\n");
-				} else{
-					PrintMessage("Todavia no se establecio la configuracion del ticket", "Atencion");
-				}
-				break;
-			case 2:
-				if (setTicket){
-					int selection;
-					printf("\n\nATENCION\n");
-					printf("Ya se establecio la configuracion del ticket, desea reestablecerla?\n");
-					printf("1. Si\n");
-					printf("2. No\n");
+		switch (option)
+		{
+		case 1:
+			if (setTicket)
+			{
+				printf("\n");
+				PrintConfigTicket();
+				printf("\n\n");
+			}
+			else
+			{
+				PrintMessage("Todavia no se establecio la configuracion del ticket", "Atencion");
+			}
+			break;
+		case 2:
+			if (setTicket)
+			{
+				int selection;
+				printf("\n\nATENCION\n");
+				printf("Ya se establecio la configuracion del ticket, desea reestablecerla?\n");
+				printf("1. Si\n");
+				printf("2. No\n");
 
-					printf("\n");
-					printf("Ingrese una opcion: ");
+				printf("\n");
+				printf("Ingrese una opcion: ");
 
-					scanf("%d", &selection);
+				scanf("%d", &selection);
 
-					if (selection == 2) selection = 0;
+				if (selection == 2)
+					selection = 0;
 
-					if (selection){
-						SetConfigTicket();
-						pConfigTicket = GetConfigTicket();
-					} else{ printf("\nNO SE REESTABLECIO LA CONFIGURACION DEL TICKET\n\n"); }
-
-				} else{
+				if (selection)
+				{
 					SetConfigTicket();
+					pConfigTicket = GetConfigTicket();
 				}
-				// Ya se configuro el ticket
-				setTicket = 1;
-				break;
-			case 3:
-				if (setTicket){
-					printf("\n");
-					PreviewTicket();
-					printf("\n\n");
-				} else{
-					PrintMessage("Todavia no se establecio la configuracion del ticket", "Atencion");
+				else
+				{
+					printf("\nNO SE REESTABLECIO LA CONFIGURACION DEL TICKET\n\n");
 				}
-				break;
-			case 4:
-				// Vuelve al menu principal
-				break;
-			default:
-				printf("Opcion no valida\n");
+			}
+			else
+			{
+				SetConfigTicket();
+			}
+			// Ya se configuro el ticket
+			setTicket = 1;
+			break;
+		case 3:
+			if (setTicket)
+			{
+				printf("\n");
+				PreviewTicket();
+				printf("\n\n");
+			}
+			else
+			{
+				PrintMessage("Todavia no se establecio la configuracion del ticket", "Atencion");
+			}
+			break;
+		case 4:
+			// Vuelve al menu principal
+			break;
+		default:
+			printf("Opcion no valida\n");
 		}
 	} while (option != 4);
 }
-<<<<<<< HEAD
 
 // void GenerateTicket(){
 // 	// Ticket data
@@ -199,19 +219,11 @@ void TicketSettings(){
 // 	printf("TICKET GENERADO\n");
 // }
 
-=======
-/*
-void GenerateTicket(){
-	// Ticket data
-	printf("GENERANDO UN NUEVO TICKET\n\n");
-	InsertTicket();
-	printf("TICKET GENERADO\n");
-}
-*/
->>>>>>> 01f5db30a53124c16b91a5370c6334c6d49a1c48
-void ViewAllTickets(){
+void ViewAllTickets()
+{
 	// Si no hay tickets, no se muestra nada
-	if (pTicketTree == NULL){
+	if (pTicketTree == NULL)
+	{
 		PrintMessage("No hay tickets aun", "Info");
 		return;
 	}
@@ -222,21 +234,26 @@ void ViewAllTickets(){
 }
 
 // La funcion SearchTicketTree busca un ticket en el arbol de tickets
-struct Ticket *SearchTicketTree(struct Ticket *pTicketAux, int id){
+struct Ticket *SearchTicketTree(struct Ticket *pTicketAux, int id)
+{
 	// Si el arbol esta vacio, no hay ningun ticket
-	if (pTicketAux == NULL){
+	if (pTicketAux == NULL)
+	{
 		return NULL;
 	}
 	// Si el id del ticket es igual al id del ticket que se busca, se retorna el ticket
-	if (pTicketAux->id == id){
+	if (pTicketAux->id == id)
+	{
 		return pTicketAux;
 	}
 	// Si el id del ticket es menor al id del ticket que se busca, se busca en el subarbol izquierdo
-	if (pTicketAux->id > id){
+	if (pTicketAux->id > id)
+	{
 		return SearchTicketTree(pTicketAux->left, id);
 	}
 	// Si el id del ticket es mayor al id del ticket que se busca, se busca en el subarbol derecho
-	if (pTicketAux->id < id){
+	if (pTicketAux->id < id)
+	{
 		return SearchTicketTree(pTicketAux->right, id);
 	}
 	// Si no se encuentra el ticket, se retorna NULL
@@ -245,35 +262,43 @@ struct Ticket *SearchTicketTree(struct Ticket *pTicketAux, int id){
 
 // La funcion SearchTicket busca un ticket en la lista de tickets
 // y lo retorna.
-struct Ticket *SearchTicket(int id){
+struct Ticket *SearchTicket(int id)
+{
 	struct Ticket *pTicket = NULL;
 	struct Ticket *pTicketAux = pTicketTree;
 	pTicket = SearchTicketTree(pTicketAux, id);
 	return pTicket;
 }
 
-void ViewTicket(){
+void ViewTicket()
+{
 	int id_ticket_search;
 	printf("Ingrese el ID del Ticket: ");
 	scanf("%d", &id_ticket_search);
 
 	struct Ticket *ticket_search = SearchTicket(id_ticket_search);
 
-	if (ticket_search == NULL){
+	if (ticket_search == NULL)
+	{
 		PrintMessage("Ticket no encontrado", "Error");
-	} else{
+	}
+	else
+	{
 		printf("Ticket encontrado\n");
 		PrintTicket(ticket_search);
 	}
 }
-void Exit(){
+void Exit()
+{
 	printf("Saliendo del programa...");
 	exit(0);
 }
-void MenuSystem(){
+void MenuSystem()
+{
 	int option;
 
-	do{
+	do
+	{
 		MenuTitle("MENU PRINCIPAL");
 		printf("1) Generar un Ticket\n");
 		printf("2) Ver un Ticket\n");
@@ -292,49 +317,51 @@ void MenuSystem(){
 		printf("Ingrese una opcion: ");
 		scanf("%d", &option);
 
-		switch (option){
-			case 1:
-				printf("\n");
-				// GenerateTicket();
-				pTicketTree = insert(pTicketTree);
-				printf("\n");
-				break;
-			case 2:
-				printf("\n\n");
-				ViewTicket();
-				break;
-			case 3:
-				printf("\n");
-				ViewAllTickets();
-				printf("\n");
-				break;
-			case 4:
-				printf("\n\n");
-				// AccountSettings ();
-				printf("\n\n");
-				break;
-			case 5:
-				printf("\n\n");
-				TicketSettings();
-				printf("\n\n");
-				break;
-			case 6:
-				printf("\n\n");
-				// Logout ();
-				printf("\n\n");
-				break;
-			case 7:
-				printf("\n\n");
-				Exit();
-				printf("\n\n");
-				break;
-			default:
-				PrintMessage("Opcion invalida", "Error");
+		switch (option)
+		{
+		case 1:
+			printf("\n");
+			// GenerateTicket();
+			pTicketTree = insert(pTicketTree);
+			printf("\n");
+			break;
+		case 2:
+			printf("\n\n");
+			ViewTicket();
+			break;
+		case 3:
+			printf("\n");
+			ViewAllTickets();
+			printf("\n");
+			break;
+		case 4:
+			printf("\n\n");
+			// AccountSettings ();
+			printf("\n\n");
+			break;
+		case 5:
+			printf("\n\n");
+			TicketSettings();
+			printf("\n\n");
+			break;
+		case 6:
+			printf("\n\n");
+			// Logout ();
+			printf("\n\n");
+			break;
+		case 7:
+			printf("\n\n");
+			Exit();
+			printf("\n\n");
+			break;
+		default:
+			PrintMessage("Opcion invalida", "Error");
 		}
 	} while (option != 8);
 }
 
-int main(){
+int main()
+{
 	SetListProducts(); // Inicializar la lista de productos global
 	MenuSystem();
 
